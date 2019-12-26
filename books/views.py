ce401a07d478f2767d41666abb7406b745a75174
Henry <Henryza1994@outlook.com>
2019-12-25 23:40:22 -0800

    django-api-first

---
+++ books/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView
+
+from .models import Book
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
